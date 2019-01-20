318856e41a9be7b00d5290e01712da025a37761a
suxizhi.gh <su.xizhi@yahoo.com>
2019-01-20 21:27:54 +0800

    Regression to replicate NQ.

--- reg.py
+++ reg.py
@@ -1,28 +1,40 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import pandas as pd
-
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 mypath = '/Users/SXZ/equity'
 
-df = pd.read_excel(mypath + '/data1.xlsx', sheetname='Sheet2')
+df = pd.read_excel(mypath + '/data1.xlsx', sheetname='Sheet1')
 
 df = df.set_index('Date').dropna()
+
+
+def datestr2num(s):
+    return datetime.strptime(s, "%Y-%m-%d")
+
+dfindex = pd.read_csv(mypath + '/^IXIC.csv', converters={0: datestr2num})[['Date', 'Adj Close']].set_index('Date')
+
+#import pdb; pdb.set_trace()
+
+df['NQ'] = dfindex['Adj Close']
 
 dfratio = df / df.shift(1)
 
 dfratio = dfratio.dropna()
 
-arr = dfratio.as_matrix().astype(np.float)
+nq = dfratio['NQ'].as_matrix().astype(np.float)
 
-nq = arr[:, 0]
-stock = arr[:, 1:]
+del dfratio['NQ']
 
+stock = dfratio.as_matrix().astype(np.float)
 
 reg = LinearRegression().fit(stock, nq)
 
 res = reg.coef_ / sum(reg.coef_)
+
+#import pdb; pdb.set_trace()
 
 code_list = ['MSFT', 'AAPL', 'FB', 'INTC', 'CSCO',
              'CMCSA', 'PEP', 'NFLX', 'AMGN', 'ADBE',
@@ -40,17 +52,19 @@ coef_sl = res[ind_select] / sum(res[ind_select])
 
 coef_sl = np.round(coef_sl, 2)
 
-stock_sl = df[code_select].as_matrix().astype(np.float)
-rep_res = stock_sl.dot(coef_sl)
+stock_sl = df[code_select].as_matrix().astype(np.float)[560:]
 
-error = rep_res - df['Adj Close'].as_matrix().astype(np.float)
+repl_res = stock_sl.dot(coef_sl)
+
+error = repl_res - df['NQ'].as_matrix().astype(np.float)
 
 # return comparison
-nqvalue = df['Adj Close'].as_matrix().astype(np.float)
-nq_return = nqvalue / nqvalue[0]
-rep_return = rep_res / rep_res[0]
+nqvalue = df['NQ'].as_matrix().astype(np.float)[560:]
 
-plt.plot(rep_return)
+nq_return = nqvalue / nqvalue[0]
+repl_return = repl_res / repl_res[0]
+
+plt.plot(repl_return)
 
 plt.plot(nq_return, color='b')
 
@@ -61,13 +75,7 @@ plt.show()
 
 import pdb; pdb.set_trace()
 
-# 'MSFT' 0.05, 'AAPL' 0.06, 'FB' 0.06, 'CSCO' 0.12,
-#'CMCSA' 0.05, 'PEP' 0.17, 'AMGN' 0.08, 'ADBE' 0.03
-#'PYPL' 0.04, 'TXN' 0.14, 'COST' 0.15, 'GILD' 0.05,
-#'SBUX' 0.07, 'CHTR' 0.05]
+# {'MSFT': 0.1, 'AAPL': 0.1, 'FB': 0.07, 'INTC': 0.04, 'CSCO': 0.07, 'CMCSA': 0.08, 'PEP': 0.02, 'NFLX': 0.09, 'AMGN': 0.07, 'ADBE': 0.03, 'PYPL': 0.04, 'AVGO': 0.05, 'TXN': 0.05, 'COST': 0.02, 'GILD': 0.05, 'NVDA': 0.07, 'SBUX': 0.03, 'BKNG': 0.02, 'WBA': 0.03}
 
-#array([ 0.05240678,  0.05755938,  0.06383678, -0.0061136 ,  0.12471774,
-#    0.04776778,  0.17234142, -0.00565503,  0.07673657,  0.02567368,
-#    0.04214532, -0.08221469,  0.14172636,  0.15430429,  0.04970746,
-#    -0.00818287,  0.07249325, -0.03178175,  0.00871812,  0.05107799,
-#    -0.00726496])
+# longer period result
+# {'MSFT': 0.11, 'AAPL': 0.11, 'FB': 0.08, 'INTC': 0.04, 'CSCO': 0.07, 'CMCSA': 0.06, 'PEP': 0.02, 'NFLX': 0.09, 'AMGN': 0.06, 'ADBE': 0.03, 'PYPL': 0.04, 'AVGO': 0.04, 'TXN': 0.01, 'COST': 0.04, 'GILD': 0.03, 'NVDA': 0.04, 'SBUX': 0.06, 'BKNG': 0.02, 'WBA': 0.02, 'CHTR': 0.03}
