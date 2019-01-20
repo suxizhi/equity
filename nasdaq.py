db9d665b826980f2153297ccdb429d44e93c1dc7
suxizhi.gh <su.xizhi@yahoo.com>
2019-01-20 21:26:28 +0800

    Combine index data and individual stock data.

--- nasdaq.py
+++ nasdaq.py
@@ -23,11 +23,12 @@ index = pd.read_sql(sql, db)['Date']
 
 df = pd.DataFrame(index=index, columns=code_list)
 
-code_list = ['FB', 'PYPL']
+#code_list = ['FB', 'PYPL']
 for code in code_list:
     sql = "select * from " + code
     
     df[code] = pd.read_sql(sql, db).set_index('Date')['Adj_Close']
+
 
 writer = pd.ExcelWriter(mypath + '/data{}.xlsx'.format(date.today().strftime('%m%d%Y')), engine = 'openpyxl')
 
@@ -36,5 +37,6 @@ writer.save()
 writer.close()
 
 db.close()
+
 import pdb; pdb.set_trace()
 
